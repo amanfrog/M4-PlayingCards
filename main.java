884e8c28f7a0133288b3916cb1938ae086f8222a
amanfrog <amanyfogg@gmail.com>
2026-02-18 06:33:25 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import javax.swing.SwingUtilities;
 
-public class App {
+class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame();
